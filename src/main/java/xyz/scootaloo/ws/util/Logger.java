b46132da8fd76b25df0a54094a52dd93a4456749
applesack <2383955734@qq.com>
2023-04-05 22:20:26 +0800

    feat(backend): logger

--- src/main/java/xyz/scootaloo/ws/util/Logger.java
+++ src/main/java/xyz/scootaloo/ws/util/Logger.java
@@ -6,7 +6,35 @@ package xyz.scootaloo.ws.util;
  */
 public class Logger {
 
-    public void info(String pattern, Object... args) {
+    private static final int receive = 1;
+    private static final int send    = 2;
+
+    private final int port;
+
+    public Logger(int port) {
+        this.port = port;
+    }
+
+    public void sendLog(String pattern, Object... args) {
+        consoleLog(send, pattern, args);
+    }
+
+    public void receiveLog(String pattern, Object... args) {
+        consoleLog(receive, pattern, args);
+    }
+
+    private void consoleLog(int type, String pattern, Object... args) {
+        String receiveMark = "<==";
+        String sendMark = "==>";
+        String offlineMark = "<=>";
+        String mark;
+        if (type == receive) {
+            mark = receiveMark;
+        } else {
+            mark = type == send ? sendMark : offlineMark;
+        }
+
+        pattern = String.format("[%d] %s %s", port, mark, pattern);
         System.out.printf(pattern + "\n", args);
     }
 
